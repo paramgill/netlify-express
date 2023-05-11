b7a06b552f1e2f086d290507755e8fe9cfa418b3
Paramjit Matharu <pkaur@cityofsydney.nsw.gov.au>
2023-05-11 10:53:02 +1000

    teest

--- express/server.js
+++ express/server.js
@@ -7,10 +7,14 @@ const bodyParser = require("body-parser");
 
 const { generateThumbnail } = require("./thumbnail");
 
-app.get("/", generateThumbnail);
-
 const router = express.Router();
+router.get("/", generateThumbnail);
 
+// router.get("/", (req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   res.write("<h1>Hello from Express.jstesting!</h1>");
+//   res.end();
+// });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
