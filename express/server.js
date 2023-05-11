f635e73b4a76f9bfc6eba003b91ecc68d0420906
Paramjit Matharu <pkaur@cityofsydney.nsw.gov.au>
2023-05-11 10:47:44 +1000

    Hello testfrom Expres

--- express/server.js
+++ express/server.js
@@ -10,11 +10,7 @@ const { generateThumbnail } = require("./thumbnail");
 app.get("/", generateThumbnail);
 
 const router = express.Router();
-// router.get("/", (req, res) => {
-//   res.writeHead(200, { "Content-Type": "text/html" });
-//   res.write("<h1>Hello from Express.jstesting!</h1>");
-//   res.end();
-// });
+
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
